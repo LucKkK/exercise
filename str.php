33dfc55f32a03612457cd410622fdbd091587c76
LucKkK <578147463@qq.com>
2016-08-10 17:27:28 +0800

    Delete str.php

--- str.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$name=$_POST['name'];
-	echo out($name);
-	function out ( $str , $ucfirst = true){
-    	$str = ucwords(str_replace('_', ' ', $str));
-    	$str = str_replace(' ','',lcfirst($str));
-    	 return $ucfirst ? ucfirst($str) : $str;
-	}
-?>
